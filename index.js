73f4fae57ab8ffddb7c4d250b236a934b4f6c26c
Austin Kim <ask199416@gmail.com>
2017-10-10 08:03:34 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function theBeatlesPlay(musicians,instruments) {
+  var result = []
+  for (var i=0; i < musicians.length; i++) {
+    result.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return result
+}
+
+function johnLennonFacts(lennonfacts) {
+  var testnumber = 0
+  while (testnumber<lennonfacts.length) {
+    lennonfacts[testnumber] + '!!!'
+  }
+  return lennonfacts
+}
