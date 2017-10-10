07acaac399cd49ab32db00c3526faa22858b71be
Austin Kim <ask199416@gmail.com>
2017-10-10 08:07:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function johnLennonFacts(lennonfacts) {
   var result =[]
   var testnumber = 0
   while (testnumber<lennonfacts.length) {
-    result.push(lennonfacts[testnumber] + '!!!')
+    result.push(lennonfacts[testnumber++] + '!!!')
   }
   return lennonfacts
 }
